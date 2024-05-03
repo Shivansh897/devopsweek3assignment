7af0f263e18f50015bf9639999bc8a7baecb6263
shivanshsingh01 <shivansh.singh01@nagarro.com>
2024-05-03 14:35:46 +0530

    final commit

---
+++ src/main/java/com/devopsassignment/controller/SonarQubeControllerTest.java
@@ -0,0 +1,11 @@
+package com.devopsassignment.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class SonarQubeControllerTest {
+
+     @GetMapping("/test")
+	    public String test() {
+	        return "Testing successfull!!";
+	    }
+}
